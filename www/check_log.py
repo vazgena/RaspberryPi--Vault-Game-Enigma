fd77271c66ba1f8b0b39b62f7201837675f0088f
Dmitriy <example@example.com>
2019-07-30 15:17:41 +0300

    PYTHONRASP-2: data analysis

--- www/check_log.py
+++ www/check_log.py
@@ -1,9 +1,12 @@
 
 
+import os
 import re
 import json
 from datetime import datetime
 from pymysql import InternalError, connect
+import asyncio
+import concurrent.futures
 
 dbuser = 'game'
 dbpass = 'h95d3T7SXFta'
@@ -38,6 +41,40 @@ def init_log_table():
     connection.close()
 
 
+def reverse_readline(filename, buf_size=8192):
+    """A generator that returns the lines of a file in reverse order"""
+    with open(filename) as fh:
+        segment = None
+        offset = 0
+        fh.seek(0, os.SEEK_END)
+        file_size = remaining_size = fh.tell()
+        while remaining_size > 0:
+            offset = min(file_size, offset + buf_size)
+            fh.seek(file_size - offset)
+            buffer = fh.read(min(remaining_size, buf_size))
+            remaining_size -= buf_size
+            lines = buffer.split('\n')
+            # The first line of the buffer is probably not a complete line so
+            # we'll save it and append it to the last line of the next buffer
+            # we read
+            if segment is not None:
+                # If the previous chunk starts right from the beginning of line
+                # do not concat the segment to the last line of new chunk.
+                # Instead, yield the segment first
+                if buffer[-1] != '\n':
+                    lines[-1] += segment
+                else:
+                    yield segment
+            segment = lines[0]
+            for index in range(len(lines) - 1, 0, -1):
+                if lines[index]:
+                    yield lines[index]
+        # Don't yield None if the file was empty
+        if segment is not None:
+            yield segment
+
+
+
 def insert_rows(conn, insertes):
     # insertes =[(table, fields, values)]
     insert_request_template = "INSERT INTO {0} ({1}) VALUES ({2});"
@@ -47,40 +84,82 @@ def insert_rows(conn, insertes):
         conn.execute(command_str)
 
 
+# n = 1758350
+n = 571770
+
 def parse_log(line):
     if not "ImmutableMultiDict" in line:
         return None
     split_line = line.split(' - ')
     date = datetime.strptime(split_line[0], "%Y-%m-%d %H:%M:%S,%f")
-    data_str = split_line[-1][19: -2]
+    data_str = split_line[-1][19: -1]
     data_str = data_str.replace('(', '[')
     data_str = data_str.replace(')', ']')
     data_str = data_str.replace('\'', '"')
-    data = json.loads(data_str)
-    data_dict = dict(data)
-    data_dict['timestamp'] = date
-    data_dict['avg'] = float(data_dict['avg'])
+    data_str = data_str.replace('"{', '{')
+    data_str = data_str.replace('}"', '}')
+    try:
+        data = json.loads(data_str)
+        data_dict = dict(data)
+        data_dict['timestamp'] = date
+        data_dict['avg'] = float(data_dict['avg'])
+        return data_dict
+    except BaseException as e:
+        print(data_str)
+        raise
 
-    return data_dict
-    a = 1
 
+def process_line(line):
+    data_dict = parse_log(line)
+    if data_dict['timestamp'] < datetime.strptime("2019-07-26 10:00:00,000", "%Y-%m-%d %H:%M:%S,%f"):
+        return
 
-def read_log(file_name):
     connection = data_connect()
     con = connection.cursor()
-    i = 0
-    with open(file_name, 'r') as f:
-        for line in f:
-            data_dict = parse_log(line)
-            if data_dict:
-                insert_rows(con, [('log_tracker', ('mac', 'station', 'room', 'volume', 'timestamp'),
-                                (data_dict['bt_addr'], data_dict['station'], data_dict['room'], data_dict['avg'], data_dict['timestamp'])),])
-                i += 1
-                print(i)
+    insert_rows(con, [('log_tracker', ('mac', 'station', 'room', 'volume', 'timestamp'),
+                       (data_dict['bt_addr'], data_dict['station'], data_dict['room'], data_dict['avg'],
+                        data_dict['timestamp'])), ])
+
+    connection.close()
+
+
+
+async def read_log(file_name, n_task=100):
+    connection = data_connect()
+    con = connection.cursor()
+    i = -1
+    loop = asyncio.get_event_loop()
+    tasks = []
+    # with open(file_name, 'r') as f:
+    #     for line in f:
+    #         i += 1
+    #         if i < n:
+    #             continue
+    #         task = loop.run_in_executor(None, process_line, line)
+    #         tasks.append(task)
+    #
+    #         if len(tasks) < n_task:
+    #             continue
+    #         done, tasks = await asyncio.wait(tasks, return_when=concurrent.futures.FIRST_COMPLETED)
+    #         tasks = list(tasks)
+    #         print(i)
+
+    for line in reverse_readline(file_name):
+            task = loop.run_in_executor(None, process_line, line)
+            tasks.append(task)
+
+            if len(tasks) < n_task:
+                continue
+            done, tasks = await asyncio.wait(tasks, return_when=concurrent.futures.FIRST_COMPLETED)
+            tasks = list(tasks)
+            print(i)
+    if len(tasks) > 0:
+        _, _ = await asyncio.wait(tasks, return_when=concurrent.futures.ALL_COMPLETED)
 
     connection.close()
 
 
 if __name__ == "__main__":
-    init_log_table()
-    read_log("track1.log")
+    # init_log_table()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(read_log("track.log"))
