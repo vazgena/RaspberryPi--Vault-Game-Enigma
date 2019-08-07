20c891e3e99e1da826fa167ca0b1fbce886d5d63
Dmitriy <example@example.com>
2019-08-07 20:27:23 +0300

    PYTHONRASP-2: fixed port

--- www/app.py
+++ www/app.py
@@ -3221,4 +3221,4 @@ def set_logger_file():
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
     set_logger_file()
-    socketio.run(app, host='0.0.0.0', port=8899, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
